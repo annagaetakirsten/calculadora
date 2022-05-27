f35a61e0123bd9e2e930b1e5d252158e207520f3
annagaetakirsten <100391760+annagaetakirsten@users.noreply.github.com>
2022-05-27 20:55:25 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+string opcaoDesejada;
+
+Console.Clear();
+
+Console.WriteLine("--- Calculadora 4 operações ---\n");
+
+Console.WriteLine("[+] Soma");
+Console.WriteLine("[-] Subtração");
+Console.WriteLine("[*] Multiplicação");
+Console.WriteLine("[/] Divisão");
+
+Console.Write("\nSelecione a operação desejada: ");
+
+opcaoDesejada = Console.ReadLine()!;
+
+switch (opcaoDesejada)
+{
+    case "+":
+       Calculadora.CalcularSoma();
+
+        break;
+    case "-":
+        Calculadora.CalcularSubtracao();
+    
+        break;
+    case "*":
+       Calculadora.CalcularMultiplicacao();
+
+        break;
+    case "/":
+      Calculadora.CalcularDivisao();
+      
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+}
+
+Console.WriteLine("\nObrigado por utilizar nosso programa.");
